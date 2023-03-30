c8ce23f8f537676e7f0a9e5da549c5625ec52916
yujin-kwak <yujin02371@gmail.com>
2023-03-30 14:40:14 +0900

    Comment: 최소힙 주석 추가

--- practice/자료구조_활용(스택,큐,해쉬,힙)/최소힙.py
+++ practice/자료구조_활용(스택,큐,해쉬,힙)/최소힙.py
@@ -11,4 +11,5 @@ while True:
         else:
             print(-1)
     else:
-        hq.heappush(a, N)
+        hq.heappush(a, N) # 힙 불변성을 유지하면서, item 값(N)을 heap(a)으로 푸시합니다.
+                          # 기본적으로 heapq모듈은 최소 힙 구조
