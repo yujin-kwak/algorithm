0b298f53b7cd21d7ddbb46a76711f664baea5ab6
yujin-kwak <yujin02371@gmail.com>
2023-03-30 13:12:10 +0900

    solve: 최소힙

---
+++ practice/자료구조_활용(스택,큐,해쉬,힙)/최소힙.py
@@ -0,0 +1,14 @@
+import heapq as hq
+
+a=[]
+while True:
+    N = int(input())
+    if N == -1:
+        break
+    if N == 0:
+        if a:
+            print(hq.heappop(a))
+        else:
+            print(-1)
+    else:
+        hq.heappush(a, N)
