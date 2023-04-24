7960d8980e140d1fe66fb0cb28676e34581095fb
yujin-kwak <yujin02371@gmail.com>
2023-04-24 15:17:33 +0900

    solve: 뮤직비디오(결정 알고리즘) 수정

--- practice/이분 탐색(결정알고리즘)&그리드 알고리즘/뮤직비디오(결정 알고리즘).py
+++ practice/이분 탐색(결정알고리즘)&그리드 알고리즘/뮤직비디오(결정 알고리즘).py
@@ -13,6 +13,7 @@ def count_fn(capacity):
 
 n, m = map(int, input().split()) # n: 곡의 개수, m: 곡을 담을 DVD 개수
 music = list(map(int, input().split())) # 각 곡의 길이
+max_len_music = max(music) # 곡들 중 가장 긴 것
 
 lt = 1 # 범위의 왼쪽 지점
 rt = sum(music) # 범위의 오른쪽 지점
@@ -20,7 +21,7 @@ res = 0 # DVD의 최소 용량 크기
 
 while lt <= rt:
     mid = (lt + rt) // 2
-    if count_fn(mid) <= m: # DVD m개로 n개의 곡을 담을 수 있는 경우
+    if mid >= max_len_music and count_fn(mid) <= m: # DVD 용량이 곡의 최대 길이보다 크고 DVD m개로 n개의 곡을 담을 수 있는 경우
         res = mid
         rt = mid - 1 # 해당 값보다 큰 값은 가능, 더 좋은 답 찾기
     else:
