906f6c9bac78e7458c543c97cd86dc85c28ffbad
yujin-kwak <yujin02371@gmail.com>
2024-05-08 17:08:17 +0900

    solve: SWEA 1486
    Signed-off-by: yujin-kwak <yujin02371@gmail.com>

---
+++ SWEA/1486.py
@@ -0,0 +1,22 @@
+def bfs(i, height):
+    global min_value
+    if height >= B:
+        min_value = min(min_value, height)
+        return
+    
+    if i == N:
+        return
+    
+    bfs(i+1, height + H[i])
+    bfs(i+1, height)
+
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N, B = map(int, input().split())
+    H = list(map(int,input().split()))
+
+    min_value = 200001
+    bfs(0, 0)
+    
+    print('#{} {}'.format(test_case, min_value))
