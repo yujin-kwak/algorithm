128ed77339c15d44da3b6d60cbd960440bd47e12
yujin-kwak <yujin02371@gmail.com>
2024-06-25 17:05:13 +0900

    solve: 백준 10814
    Signed-off-by: yujin-kwak <yujin02371@gmail.com>

---
+++ Baekjoon/10814.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+members = []
+for i in range(N):
+    age, name = map(str, input().split())
+    age = int(age)
+    members.append((age, name))
+
+members.sort(key=lambda x: x[0])
+
+for i in members:
+    print(i[0], i[1])
